5822b0ba8ed94be311d45ccf29f7f77eb6f95924
Nicolas Mayorga <nmayorgav@gmail.com>
2023-11-17 01:03:38 -0600

    Delete trash app

--- task/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Task(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    completed_at = models.DateTimeField(null=True, blank=True)
